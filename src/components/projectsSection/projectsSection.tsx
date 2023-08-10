573135948f6b168ef0237be27e29ec4b162c8f49
UserNaneless <vgripor@gmail.com>
2023-08-10 21:14:06 +0500

    Update projectsSection.tsx

--- src/components/projectsSection/projectsSection.tsx
+++ src/components/projectsSection/projectsSection.tsx
@@ -14,7 +14,7 @@ export default class Projects extends Component {
                         title: "Vision Agency",
                         stack: ["HTML", "React", "CSS",],
                         mainText: "Landing page with animations for the Vision Agency",
-                        githubLink: "https://github.com/UserNaneless/Portfolio/tree/main/site",
+                        githubLink: "https://github.com/UserNaneless/VisionLanding",
                         liveLink: "https://vision-landing.vercel.app/"
                     }} />
 
@@ -23,7 +23,7 @@ export default class Projects extends Component {
                         title: "Weather App",
                         stack: ["HTML", "React", "CSS", "REST API"],
                         mainText: "Weather App to practice using REST API. Your can fetch any weather by city name",
-                        githubLink: "https://github.com/UserNaneless/Portfolio/tree/main/site3",
+                        githubLink: "https://github.com/UserNaneless/WeatherApi",
                         liveLink: "https://weather-api-rho-one.vercel.app/"
                     }} />
 
@@ -32,7 +32,7 @@ export default class Projects extends Component {
                         title: "Music Visualizer / Player with playlist",
                         stack: ["HTML", "React", "CSS", "REST API", "Next", "Web Audio API", "NodeJS", "Express"],
                         mainText: "Audio Visualizer working on NodeJS server. To play something write in playlist/video link from youtube and click on blue background. Fully functional playlist: queue clearing, looping, shuffling, drag and drop.",
-                        githubLink: "https://github.com/UserNaneless/Portfolio/tree/main/site4client",
+                        githubLink: "https://github.com/UserNaneless/Visualizer",
                         liveLink: "https://visualizer-eight.vercel.app/"
                     }} />
 
@@ -41,7 +41,7 @@ export default class Projects extends Component {
                         title: "Online Gaming/Streaming Platform Cyborg",
                         stack: ["HTML", "React", "CSS", "React-Router-Dom", "Prisma"],
                         mainText: "Cyberg is an online gaming shop with streaming capabilities. Profile, game details, top games, top streamers: everything you need is here! ",
-                        githubLink: "https://github.com/UserNaneless/Portfolio/tree/main/site5",
+                        githubLink: "https://github.com/UserNaneless/CyborgShop",
                         liveLink: "https://cyborg-shop.vercel.app/"
                     }} />
 
@@ -50,7 +50,7 @@ export default class Projects extends Component {
                         title: "Tutoring Site",
                         stack: ["HTML", "React", "CSS", "Next", "Prisma", "Next-Auth"],
                         mainText: "Simple tutoring site for my needs. There is a live lesson room, homework room, profile room. Live-Lesson room - is live tutoring with a drawing board. Homework room - you have a set of tasks to do, solutions can be pended as LaTeX/photo. Profile - your statistics.",
-                        githubLink: "#"
+                        githubLink: "https://github.com/UserNaneless/Portfolio"
                     }} />
 
                 </div>
